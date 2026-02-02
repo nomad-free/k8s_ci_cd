21088b1ad87c633148e7e8ebee3fb582c96faee4
englishtour0224 <englishtour0224@gmail.com>
2026-02-02 17:55:13 +0900

    주석 달음

--- terraform/ingress-nginx.tf
+++ terraform/ingress-nginx.tf
@@ -15,8 +15,12 @@ resource "helm_release" "ingress_nginx" {
         type = "LoadBalancer"
         annotations = {
           # [2025 Best Practice] AWS Load Balancer Controller v3.x 호환 설정
-          "service.beta.kubernetes.io/aws-load-balancer-type"            = "external"
-          "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
+          "service.beta.kubernetes.io/aws-load-balancer-type"   = "external"
+          "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
+          # 3. 타겟 유형 (성능 핵심)
+          # "ip": NLB가 트래픽을 노드(EC2)를 거치지 않고 "파드(Pod)의 IP"로 직접 꽂아줍니다.
+          # - 장점: 지연 시간(Latency) 감소, 불필요한 네트워크 홉 제거.
+          # - 조건: Amazon VPC CNI를 사용해야 함 (앞서 설정한 vpc-cni 설정과 연결됨).
           "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
         }
       }
@@ -39,8 +43,11 @@ resource "helm_release" "ingress_nginx" {
       # [2025 표준] 고가용성을 위한 토폴로지 분산 제약 조건 추가
       topologySpreadConstraints = [
         {
-          maxSkew           = 1
-          topologyKey       = "topology.kubernetes.io/zone"
+          # 각 존(Zone) 간의 파드 개수 차이가 1개를 넘지 않도록 합니다. (균등 분배)
+          maxSkew = 1
+          # 파드들을 AWS의 **서로 다른 가용 영역(AZ, 예: ap-northeast-2a, 2c)**에 골고루 퍼뜨립니다.
+          topologyKey = "topology.kubernetes.io/zone"
+          # 만약 균등하게 배포할 수 없는 상황(예: 한쪽 존 장애)이라면, 억지로 한쪽에 몰아넣지 말고 "배포를 대기(Pending)" 시키라는 강력한 제약입니다. (상황에 따라 ScheduleAnyway를 쓰기도 하지만, 여기선 엄격한 HA를 추구함)
           whenUnsatisfiable = "DoNotSchedule"
           labelSelector = {
             matchLabels = {
