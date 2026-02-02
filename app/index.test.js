7c9faf92b8bc0bce5aae744bb5ed2785f16184ed
englishtour0224 <englishtour0224@gmail.com>
2026-02-02 22:19:04 +0900

    minor: changes from single quote to double quote

--- app/index.test.js
+++ app/index.test.js
@@ -1,13 +1,11 @@
-
-const app = require('./index');
-describe('Health Check', () => {
-  test('should return healthy status', async () => {
-    
+const app = require("./index");
+describe("Health Check", () => {
+  test("should return healthy status", async () => {
     expect(true).toBe(true);
   });
 });
-describe('App', () => {
-  test('should be defined', () => {
+describe("App", () => {
+  test("should be defined", () => {
     expect(app).toBeDefined();
   });
 });
